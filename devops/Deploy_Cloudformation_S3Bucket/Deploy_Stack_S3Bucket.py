54ac5bae609af0d3100c0139a48dde45cd845240
glanzmichael25 <glanz.michael.25@gmail.com>
2022-01-30 16:14:22 -0800

    Commit Updates To Template and Deploy Script
    Updates previous StackTemplate json file and replaces Deploy_Cloudformation_Stack_S3Bucket.py with Deploy_Stack_S3Bucket.py

--- devops/Deploy_Cloudformation_S3Bucket/Deploy_Stack_S3Bucket.py
+++ devops/Deploy_Cloudformation_S3Bucket/Deploy_Stack_S3Bucket.py
@@ -12,13 +12,11 @@ stack_template = ''
 with open(f"{script_dir}/StackTemplate.json", 'r') as fd:
 	stack_template = fd.read()
 
-#print(stack_template)
 print("Deploys a Stack and S3 Bucket To Cloudformation.\n")
 
 patternAlpha = re.compile("[A-Za-z]+")
-patternNum = re.compile("[0-9]+")
 
-#User Input Asks For Initials To Use In Stack Name
+#Asks For User Initials
 initials = ''
 while initials != 'q':
     initials = input('Please Enter Your Initials, Or Press "Q" To Quit: ')
@@ -35,10 +33,11 @@ while initials != 'q':
         print("Error: Initials Must Not Be Longer Than 3 characters.\n")
     else:
         print("\nYour Initials Entered: " + initials + "\n")
-        print("Your Stack Name Will Be " + "'" + initials + "-stack'" + "." + "\n")
-        print("Please Wait While Your Stack and S3 Bucket Are Being Deployed To Cloudformation, Thank You.\n")
+        print("Your Stack Will Be Named: " + "'" + initials + "-stack'\n")
+        print("Please Wait While Your Stack and S3Bucket Are Being Deployed To Cloudformation.")
+        print("Waiting...\n")
         break
-
+       
 #Uses User Initials For Stack Name
 stack_name = (initials + '-stack')
 
@@ -46,12 +45,17 @@ stack_name = (initials + '-stack')
 cf_client = boto3.client('cloudformation')
 
 #Checks If Stack Already Exists
-stacks = cf_client.list_stacks()['StackSummaries']
-for stack in stacks:
-    if stack_name == stack['StackName']:
-        print("Error: This Stack Already Exists, Please Try Again.\n")
-        exit()
-    else:
-        #Creates New Stack and S3 Bucket From Template
-        cf_client.create_stack(StackName=stack_name, TemplateBody=stack_template)
+paginator = cf_client.get_paginator('list_stacks')
+for page in paginator.paginate():
+    for stack in page['StackSummaries']:
+        if stack['StackStatus'] == 'DELETE_COMPLETE':
+            continue
+        if stack['StackName'] == stack_name:
+            print("Error: This Stack Already Exists, Please Try Again.\n")
+            exit()
 
+#Deploys New Stack And S3Bucket       
+stack_result = cf_client.create_stack(StackName=stack_name, TemplateBody=stack_template)
+waiter = cf_client.get_waiter('stack_create_complete')
+waiter.wait(StackName=stack_name)
+print("Congradulations! Your Stack " + stack_name + " And S3Bucket Is Now Completed.\n")
