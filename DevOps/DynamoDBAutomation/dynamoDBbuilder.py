2292a4c688b0dca788b9c7cd2b91d24bb577da5d
Dagmawi Girma Demissie <dagimethiopia17@gmail.com>
2022-01-31 20:21:31 -0800

    Developed a script using (boto3 + cloud formation) to create a DynamoDB resource for developers.

---
+++ DevOps/DynamoDBAutomation/dynamoDBbuilder.py
@@ -0,0 +1,49 @@
+# reads the template.json file and creates a stack
+import json
+import boto3
+
+
+# wrap the operation in a function
+
+def create_dynamodb_table(tableName, keyName, cfTemplate):
+    """Given a table name, key name and CFT template, it'll create a DynamoDB table.
+
+    Keyword arguments:
+    tableName -- the DyanmoDB table name
+    keyName -- the partion key name
+    cfTemplate -- the file path to the cloud formation template json
+    """
+    cfnclient = boto3.client('cloudformation')
+
+    # open the template file
+    with open(cfTemplate) as fd:
+        template = json.load(fd)
+
+    # update the template with the table name and hash key name
+    template["Resources"]["DynamoDBTable"]["Properties"]["TableName"] = tableName
+    template["Resources"]["DynamoDBTable"]["Properties"]["AttributeDefinitions"][0]["AttributeName"] = keyName
+    template["Resources"]["DynamoDBTable"]["Properties"]["KeySchema"][0]["AttributeName"] = keyName
+
+
+# create the stack
+    cfnclient.create_stack(StackName=tableName,
+                           TemplateBody=json.dumps(template)
+                           )
+
+# wait for the stack to complete
+    waiter = cfnclient.get_waiter('stack_create_complete')
+    waiter.wait(StackName=tableName)
+
+# get the stack output
+    stack = cfnclient.describe_stacks(StackName=tableName)
+    table_status = stack['Stacks'][0]['StackStatus']
+
+# return the table status
+    return table_status
+
+
+# test the function
+# if __name__ == '__main__':
+#     table_status = create_dynamodb_table(
+#         'testDynamoDbTable4', 'id', './devops/DynamoDBAutomation/createDynamoDB.json')
+#     print(table_status)
