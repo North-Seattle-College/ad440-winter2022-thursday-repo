2e22cf9afa62321f25a10b7ab192933706f6768d
Hanna-N9 <hannyhabte@yahoo.com>
2022-02-04 19:27:02 -0800

    Added codes to InputBox.js & InputBox.css

--- frontend/src/components/InputBox/InputBox.js
+++ frontend/src/components/InputBox/InputBox.js
@@ -1,5 +1,15 @@
+import "./InputBox.css";
+
 export default function InputBox() {
-    return (
-        <div>InputBox</div>
-    );
+  return (
+    <div>
+      <div>
+        <h1>Feedback</h1>
+      </div>
+
+      <div className="third">To add description in future</div>
+      <h2>Please enter a comment</h2>
+      <input type="text" className="feedback-input" />
+    </div>
+  );
 }
