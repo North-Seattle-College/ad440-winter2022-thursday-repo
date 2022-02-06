3d71a7bde867fa9e719918d8a440c8e5273d2208
Payam Taherirostami <payamtaherirostami@gmail.com>
2022-02-05 16:11:14 -0800

    Adding submit button

--- frontend/src/components/SubmitBtn/SubmitBtn.js
+++ frontend/src/components/SubmitBtn/SubmitBtn.js
@@ -1,5 +1,18 @@
+import React, { useState } from "react";
+import './SubmitBtn.css'
 export default function SubmitBtn() {
+
+    const [feedback, setFeedback] = useState("");
+    
+    const handleSubmit = (evt) => {
+        evt.preventDefault();
+        alert(`You entered:  ${feedback}`)
+        setFeedback('')
+    }
     return (
-        <div>SubmitBtn</div>
+
+        <div>
+             <button onClick={handleSubmit}>Submit</button>
+        </div>
     );
 }
