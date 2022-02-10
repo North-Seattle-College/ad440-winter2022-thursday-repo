6af66eb0a02c5226b9116293ccc96909fb9decc8
Hanna-N9 <hannyhabte@yahoo.com>
2022-02-10 14:52:40 -0800

    Added a brief description to a textbox

--- frontend/src/components/InputBox/InputBox.js
+++ frontend/src/components/InputBox/InputBox.js
@@ -1,17 +1,24 @@
 import "./InputBox.css";
 
-export default function InputBox({feedback, setFeedback}) {
+export default function InputBox({ feedback, setFeedback }) {
   return (
     <div>
       <div>
         <h1>Feedback</h1>
       </div>
 
-      <div className="third">To add description in future</div>
+      <div className="third">
+        As you write down messages to give feedback to students, You will
+        receive relevant coaching messages according to your feedback to improve
+        it.
+      </div>
       <h2>Please enter a comment</h2>
-      <input type="text" className="feedback-input"   
-             value={feedback}
-             onChange={e => setFeedback(e.target.value)}/>
+      <input
+        type="text"
+        className="feedback-input"
+        value={feedback}
+        onChange={e => setFeedback(e.target.value)}
+      />
     </div>
   );
 }
