ad0d5a0a46ac761df4a071a526062081f87a2373
David Nguyen <davidnguyen234@gmail.com>
2022-02-10 13:26:25 -0800

    add feedback box

--- frontend/src/components/FeedbackBox/FeedbackBox.js
+++ frontend/src/components/FeedbackBox/FeedbackBox.js
@@ -1,5 +1,12 @@
-export default function FeedbackBox() {
+import "./Feedback.css"
+
+export default function FeedbackBox({feedback , setFeedback}) {
     return (
-        <div>FeedbackBox</div>
+    <div>
+            <div className="analysis"><h2>Analysis</h2></div>
+            <div className="feedbackbox">
+            <div className="feedback"></div>
+        </div>
+    </div>
     );
 }
