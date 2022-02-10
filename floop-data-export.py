08dd81d7465bd5d0c70ef55798ad00fbf057bb45
ZenSorcere <zen.sorcere@gmail.com>
2022-02-10 13:26:27 -0800

    adds filepath CL arg error handling and graceful exit

--- floop-data-export.py
+++ floop-data-export.py
@@ -5,9 +5,8 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 import json
-import os
 
-# Initial test of Argparse and adding command line arguments
+# Initiate Argparse for command line arguments and instructions for '--help'.
 parser = argparse.ArgumentParser(description='Using the data-export script.')
 parser.add_argument("fp", type=str,
                     help="1st arg: This is the credentials filepath variable")
@@ -19,41 +18,38 @@ fp = args.fp
 dbase = args.db
 print('filepath: ' + fp + '\ndatabase: ' + dbase)
 
-gPath = os.path.isfile(fp)
+# Try clause to handle exceptions should firebase credentials path not be valid.
+#    On Exception, will display clear instructions and exit gracefully.
+#    On success, will initialize floop db connection, and pull feedback comments.
+try:
+    cred = credentials.Certificate(fp)
+    pass
+except Exception as e:
+    print("Oops!", e.__class__, "occurred.")
+    sys.exit('File not found, check Floop credentials file path and try again.')
+else:
+    firebase_admin.initialize_app(cred)
+    
+    db = firestore.client()
 
-# Get path of firebase cert from user. Prompt user if path doesnt exist
-# while True:
-    # certLoc = input('Enter full path of Floop Firebase cert file: ')
-    # gPath = os.path.isfile(fp)
-
-if not gPath:
-    print('File not found, check Floop credentials file path and try again.')
-
-# Use cert file to initialize app access and
-#   connect to Dev_Database Conversations collection
-cred = credentials.Certificate(fp)
-firebase_admin.initialize_app(cred)
-
-db = firestore.client()
-
-collection = 'Databases/{}_Database/Conversations'.format(dbase)
-conditionals = ['What is your goal for this year?',
+    collection = 'Databases/{}_Database/Conversations'.format(dbase)
+    conditionals = ['What is your goal for this year?',
                 'Audio Comment', 'Freeform', 'Freeform Comment', '', ' ']
-print('collection: '+ collection)
-# pull conversation documents where 'Comment_Preview' field isn't certain
+    
+# Pull conversation documents where 'Comment_Preview' field isn't certain
 #   text, and pulling a limit of 10k.
-conversations = db.collection(collection).where(
-    'Comment_Preview', 'not-in', conditionals).limit(10).get()
+    conversations = db.collection(collection).where(
+        'Comment_Preview', 'not-in', conditionals).limit(10).get()
 
 if __name__ == '__main__':
 
-    # Iniialize empty list, "cList"
+# Iniialize empty list, "cList"
     cList = []
+    
 # For each item in Conversations, ea entry has a Messages collection
 # all documents will be ordered by 'Date_Submitted' and only the first comment will be pulled.
 # For each document in the pulled list(currently limited to 1 per collection),
 #    get value of "Text" field and add to cList
-
     for convo in conversations:
         convo_entries = convo.reference.collection(
             'Messages').order_by(u'Date_Submitted').limit(1).get()
@@ -67,21 +63,11 @@ if __name__ == '__main__':
     arr = set(cList)
     new_arr = list(arr)
 
-print(new_arr)
-# Write contents of list to json file
+# Initiate s3 connection to desired bucket and default object filename.
+    s3 = boto3.resource('s3')
+    s3object = s3.Object('ad440-mpg-floop-export-storage', 'auto-export-test.json')
 
-""" s3 = boto3.client('s3')
-filename = "upload-test-boto3client.json"
-with open(filename, "w") as f:
-    json.dump(new_arr, f)
-    s3.upload_file(filename,
-        'ad440-mpg-floop-export-storage', filename)
- """
-
-
-s3 = boto3.resource('s3')
-s3object = s3.Object('ad440-mpg-floop-export-storage', 'auto-export-test.json')
-
-s3object.put(
-    Body=(bytes(json.dumps(new_arr).encode('UTF-8'))), ContentType='application/json'
-)
+# Put json of list into object and put into s3 bucket.
+    s3object.put(
+        Body=(bytes(json.dumps(new_arr).encode('UTF-8'))), ContentType='application/json'
+    )
