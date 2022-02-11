3d182168a34ef1c23885e9d0e46c81285879e412
Hanna-N9 <hannyhabte@yahoo.com>
2022-02-11 01:52:57 -0800

    Adjusted the phrases in InputBox.js

--- frontend/src/components/InputBox/InputBox.js
+++ frontend/src/components/InputBox/InputBox.js
@@ -8,15 +8,15 @@ export default function InputBox({ feedback, setFeedback }) {
       </div>
 
       <div className="description">
-        As you write down messages to give feedback to students, you will
+        As you write down your comments to give feedback to students, you will
         receive relevant coaching messages according to your feedback to improve
         it.
       </div>
-      <h2>Please enter a comment</h2>
+      <h2>Teacher Comment</h2>
       <input
         type="text"
         className="feedback-input"
-        placeholder="Your feedback goes here"
+        placeholder="Your comment goes here..."
         value={feedback}
         onChange={e => setFeedback(e.target.value)}
       />
