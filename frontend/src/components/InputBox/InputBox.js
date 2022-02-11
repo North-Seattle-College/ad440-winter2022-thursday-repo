be832899e2e4c17c3be53b0c491b242321222376
Payam Taherirostami <payamtaherirostami@gmail.com>
2022-02-10 19:11:13 -0800

    adding submit

--- frontend/src/components/InputBox/InputBox.js
+++ frontend/src/components/InputBox/InputBox.js
@@ -1,17 +1,7 @@
 import "./InputBox.css";
 
-export default function InputBox({feedback, setFeedback}) {
+export default function InputBox() {
   return (
-    <div>
-      <div>
-        <h1>Feedback</h1>
-      </div>
-
-      <div className="third">To add description in future</div>
-      <h2>Please enter a comment</h2>
-      <input type="text" className="feedback-input"   
-             value={feedback}
-             onChange={e => setFeedback(e.target.value)}/>
-    </div>
+      <div>InputBox</div>
   );
 }
