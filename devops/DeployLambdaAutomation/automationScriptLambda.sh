058904fa165a790dda4d4f8aaa314ca60418efd4
Biks <bikasjthapa@gmail.com>
2022-02-15 20:06:36 -0800

    Updated script to take parameters and changed the template from json to yml

--- devops/DeployLambdaAutomation/automationScriptLambda.sh
+++ devops/DeployLambdaAutomation/automationScriptLambda.sh
@@ -1,56 +1,42 @@
 # !/bin/sh
-# Ask user for the input
-USER_INPUT="Please enter initials of your first and last name (or 'e' to exit)"
 NUM=2
+input=
+#Parse argument
+while [ "$1" != "" ]; do
+    case $1 in
+        --initials )
+            shift
+            input="$1"
+        ;;
+    esac
+    shift
+done
 
-while :
-  do
-    # get and validate user initials
-    printf "%s\n" "$USER_INPUT" 
-    while read input && [ "$input" != "e" ];
-      do
-        # Validates the input is null or is of 0 length
-        if [ -z "$input" ]; then
-          printf '%s\n' "Error: initials can't be empty" 
-        # checks to see if the string matches the requested pattern
-        #elif [[ "${input}" =~ [^a-zA-Z] ]]; then
-          #printf '%s\n' "Error: initials must only contain letters, a-z"
-        elif [[ "${#input}" != $NUM ]]; then
-          printf '%s\n' "Error: initials must be $NUM characters"
-        else
-          break
-        fi
-          printf "%s\n" "$USER_INPUT" 
-      done
+# Validates the input is null or is of 0 length
+if [ -z "$input" ]; then
+    printf '%s\n' "Error: initials can't be empty"
+    exit 1
+    #checks to see if the string matches the requested pattern
+    elif [[ "${input}" =~ [^a-zA-Z] ]]; then
+    printf '%s\n' "Error: initials must only contain letters, a-z"
+    exit 1
+    elif [[ "${#input}" != $NUM ]]; then
+    printf '%s\n' "Error: initials must be $NUM characters"
+    exit 1
+fi
 
-    if [ "$input" == "e" ]; then
-      printf '%s\n' "Done"
-      break
-    fi
+# Removes spaces if any and change to lower
+formattedInitials="$(echo "${input}" | tr -d '[:space:]' | tr '[:upper:]' '[:lower:]')"
+todayDate=$(date +%Y%m%d%S)
+randomString=$(cat /dev/urandom | tr -dc '[:alpha:]' | fold -w ${1:-9} | head -n 1)
+commonName=$formattedInitials-$randomString-$todayDate
+lambdaName=$commonName-lambda
+stackName=$commonName-stack
+lambdaTemplate="./cloudFormationLambdaTemplate.yml"
+printf 'ProgramRunning'
+# The below lines creates the lambda and stack in AWS
+aws cloudformation deploy --template-file $lambdaTemplate --stack-name $stackName --parameter-overrides LambdaName=$lambdaName --capabilities CAPABILITY_NAMED_IAM
 
-    # Removes spaces ifany and change to lower
-    formattedInitials="$(echo "${input}" | tr -d '[:space:]' | tr '[:upper:]' '[:lower:]')"
-    uniqueName=$formattedInitials
-    stackName=$uniqueName-stack
 
-    printf "%s\n" "Your stack will be named as follows:" \
-            "stack: $stackName" \
-            "Create lambda function and stack? (y/n)" 
 
-     #Importing the lamda template to create the function
-    lambdaTemplate="./CloudFormationLambdaTemplate.json"
-    while read userResponse && [ "$userResponse" != "e" ];
-      do
-        if [ "$userResponse" == "y" ]; then
-          printf '%s\n' "aws cloudformation deploy --template-file $lambdaTemplate --stack-name $stackName --capabilities CAPABILITY_NAMED_IAM"
-          aws cloudformation deploy --template-file $lambdaTemplate --stack-name $stackName --capabilities CAPABILITY_NAMED_IAM
-          break
-        elif [ "$userResponse" == "n" ]; then
-          break
-        else
-          printf "%s\n" "Create lambda? (y/n)"
-        fi
-      done
-    printf '%s\n' "Done" 
-    break
-  done 
+
