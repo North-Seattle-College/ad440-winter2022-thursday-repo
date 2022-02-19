17b9b7d6240f11bb1dfe669700aa5df8a9dccad1
Hanna-N9 <hannyhabte@yahoo.com>
2022-02-18 19:41:43 -0800

    Post Request for submit button to call API

--- frontend/src/components/SubmitBtn/SubmitBtn.js
+++ frontend/src/components/SubmitBtn/SubmitBtn.js
@@ -1,12 +1,25 @@
-// Task for Payam Taherirostami
-export default function SubmitBtn({ feedback, setFeedback, setShow }) {
-  const handleSubmit = (evt) => {
+export default function SubmitBtn({ feedback, setShow }) {
+  const url =
+    "https://9u4xt4nqr1.execute-api.us-west-2.amazonaws.com/default/test";
+
+  const handleSubmit = evt => {
     evt.preventDefault();
+    fetch(url, {
+      method: "POST",
+      body: JSON.stringify({ feedback }), // convert to JSON
+      headers: { "Content-Type": "application/json" }, // get the response data in that format
+    })
+      .then(response => response.json())
+      .then(res => console.log(res)) // response shown on the console window
+      .catch(e => console.error("Error indicated:", e));
     setShow(true);
-  }
+  };
+
   return (
     <div>
-      <button onClick={handleSubmit}>Submit</button>
+      <button disabled={!feedback} onClick={handleSubmit}>
+        Submit
+      </button>
     </div>
   );
 }
