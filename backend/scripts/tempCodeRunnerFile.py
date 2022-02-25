6f63ce5d09cd92643c85d3d8dea67fbceeca7f41
ZenSorcere <zen.sorcere@gmail.com>
2022-02-25 15:09:42 -0800

    removes tempCodeRunnerFile.py anamoly

--- backend/scripts/tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-to be replaced with question/statement,
-#   positive/negative/neutral, and emotion matching later...?
-question = 'Test1'
-posNeg = ''
-emotion = 'Test4'
-
-# Discarded alt option for posNeg
-#     tb = Blobber(analyzer=NaiveBayesAnalyzer(
