1c36bab865e80a74cd127a293843f21f64874030
glanzmichael25 <glanz.michael.25@gmail.com>
2022-03-02 16:54:34 -0800

    commit

--- devops/DeployCloudformationS3Bucket/deployStackS3Bucket.py
+++ devops/DeployCloudformationS3Bucket/deployStackS3Bucket.py
@@ -8,9 +8,22 @@ import argparse
 import random
 import string
 import subprocess
+import logging
 from datetime import date
 from os.path import dirname
 script_dir = dirname(__file__)
+
+#Sets logging
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+formatter = logging.Formatter('%(asctime)s:%(levelname)s:%(name)s:%(message)s')
+formatter = logging.Formatter(fmt='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
+file_handler = logging.FileHandler('deployStackS3Bucket.log')
+file_handler.setFormatter(formatter)
+streamHandler = logging.StreamHandler(sys.stdout)
+streamHandler.setLevel(logging.INFO)
+logger.addHandler(file_handler)
+logger.addHandler(streamHandler)
 
 #Creates cloudformation client
 cf_client = boto3.client('cloudformation', region_name='us-west-2')
@@ -33,7 +46,7 @@ def main(input_initials):
 
     #Gets initials
     initials = args.input_initials
-    print("\nYour Initials Entered: " + initials + "\n")
+    logger.info("Your Initials Entered: " + initials + "\n")
 
     #Gererates unique id
     unique_id = ''.join(random.SystemRandom().choice(string.ascii_lowercase + string.digits) for _ in range(5))
@@ -57,31 +70,31 @@ def main(input_initials):
     #Verifies Initials
     patternAlpha = re.compile("[A-Za-z]+")
     if patternAlpha.fullmatch(initials)==None:
-        print("Error: Initials Must Contain Alphabetic Characters Only.\n") 
+        logger.error("Initials Must Contain Alphabetic Characters Only.\n")
     elif len(initials) == '':
-        print("Error: Initials Must Not Be Empty.\n")
+        logger.error("Initials Must Not Be Empty.\n")
     elif len(initials) < 2:
-        print("Error: Initials Must Be At Least 2 characters Long.\n")
+        logger.error("Initials Must Be At Least 2 characters Long.\n")
     elif len(initials) > 5:
-        print("Error: Initials Must Not Be More Than 5 characters Long.\n")  
+        logger.error("Initials Must Not Be More Than 5 characters Long.\n")
     else:
-        print("Your Stack Will Be Named: " + "'" + stack_name + "'\n")
-        print("Your S3Bucket Will Be Named: " + "'" + bucket_name + "'\n")
-        print("Please Wait While Your Stack and S3Bucket Are Being Deployed To Cloudformation.")
-        print("Waiting...\n")
+        logger.info("Your Stack Will Be Named: " + "'" + stack_name + "'\n")
+        logger.info("Your S3Bucket Will Be Named: " + "'" + bucket_name + "'\n")
+        logger.info("Please Wait While Your Stack and S3Bucket Are Being Deployed To Cloudformation.\n")
+        logger.info("Waiting...\n")
         #Checks if stack already exists
         if _stack_exists(stack_name):
-            print("Error: A Stack Named " + "'" + stack_name + "' Already Exists, Please Try Again.\n")
+            logger.error("A Stack Named " + "'" + stack_name + "' Already Exists, Please Try Again.\n")
         else:
             #Creates stack and S3bucket
             stack_result = cf_client.create_stack(StackName=stack_name, TemplateBody=stack_template, Parameters=parameters)
             waiter = cf_client.get_waiter('stack_create_complete')
             waiter.wait(StackName=stack_name)
-            print("Congradulations! Your Stack " + stack_name + " And S3Bucket " + bucket_name + " Are Now Completed.\n")
-
+            logger.info("Congradulations! Your Stack " + stack_name + " And S3Bucket " + bucket_name + " Are Now Completed.\n")
             #Echos bucket name as Github environment variable
             echo_arg = ("echo BUCKET_NAME=" + bucket_name + " >> $GITHUB_ENV")
             subprocess.Popen(echo_arg, shell=True)
+            logger.debug(echo_arg)
 
 #Reads in stack template file
 def _stack_template_file():
