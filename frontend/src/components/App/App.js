f727d40ab72d7b5062daf96edb89d688bdf84937
David Nguyen <davidnguyen234@gmail.com>
2022-03-04 01:32:15 -0800

    create error box component

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -4,28 +4,30 @@ import FeedbackBox from "../FeedbackBox/FeedbackBox";
 import ClearBtn from "../ClearBtn/ClearBtn";
 import SubmitBtn from "../SubmitBtn/SubmitBtn";
 import React, { useState } from "react";
-import logo from "../Logo/floop_logo.png"
+import logo from "../Logo/floop_logo.png";
+import ErrorBox from "../ErrorBox/ErrorBox";
 
 function App() {
   const [input, setInput] = useState('');
   const [AIfeedback, setAIfeedback] = useState();
   const [show, setShow] = useState(false);
+  const [error, setError] = useState(false);
   return (
 
     <div className="App">
       <img className="logo" src={logo} alt="Logo" />
       <div id="wrapper">
-        <InputBox input={input} setInput={setInput} setShow={setShow} />
+        <InputBox input={input} setInput={setInput} setShow={setShow} setError={setError}/>
         <div className="userBtns">
           <div className="clearBtn">
-            <ClearBtn setInput={setInput} setShow={setShow} />
+            <ClearBtn setInput={setInput} setShow={setShow} setError={setError}/>
           </div>
           <div className="submitBtn">
-            <SubmitBtn input={input} setInput={setInput} setShow={setShow} setAIfeedback={setAIfeedback} />
+            <SubmitBtn input={input} setInput={setInput} setShow={setShow} setAIfeedback={setAIfeedback} setError={setError}/>
           </div>
         </div>
         <div id="feedbackTitle"></div>
-        {show && <FeedbackBox AIfeedback={AIfeedback} show={show} />}
+        {show? <FeedbackBox AIfeedback={AIfeedback} show={show} /> : <ErrorBox  error={error} />}
       </div>
     </div>
   );
---
+++ frontend/src/components/ErrorBox/ErrorBox.js
@@ -0,0 +1,18 @@
+import { usePromiseTracker } from "react-promise-tracker";
+import ReactLoading from 'react-loading';
+
+export default function ErrorBox() {
+  const { promiseInProgress } = usePromiseTracker();
+
+  const LoadingIndicator = () => {
+    return (
+      <div className='loadingIcon'>
+        <ReactLoading type={'spin'} color={'#00807F'} height={'9%'} width={'9%'} />
+      </div>  
+    );
+  }
+
+  
+  return promiseInProgress ? 
+  <LoadingIndicator /> : <div><div><h4>Error:</h4></div><div className="feedback">  ❌  "ERROR request failed"</div></div>
+}
