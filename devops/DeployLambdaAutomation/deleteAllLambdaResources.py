7728ecf2c90d568af046bff99ff7de60ff3ce539
Biks <bikasjthapa@gmail.com>
2022-03-16 15:24:05 -0700

    Updated comments

--- devops/DeployLambdaAutomation/deleteAllLambdaResources.py
+++ devops/DeployLambdaAutomation/deleteAllLambdaResources.py
@@ -5,27 +5,27 @@ import argparse
 
 
 def main():
-    #Main funnction that lists all tables in the specified aws region and propmpts the user to delete a table.
+    #Main funnction that lists all functions in the specified aws region and propmpts the user to delete a function.
     parser = argparse.ArgumentParser()
     parser.add_argument('--region', type=str, required=True,
-                        help='AWS region where the table is located')
+                        help='AWS region where the functions are located')
     parser.add_argument('--functionName', type=str,
-                        help='The name of the table to be deleted')
+                        help='The name of the lambda function to be deleted')
     parser.add_argument('--cleanAllLambda', action='store_true',
-                        help='warning: This will force delete all the tables in the selected region')
+                        help='warning: This will force delete all the lambda functions in the selected region')
     args = parser.parse_args()
-    # connect to aws and list all the tables in the selected region
+    # connect to aws and list all the functions in the selected region
     client = boto3.client('lambda', region_name=args.region)
     response = client.list_functions()
 
-  # print the table names in the selected region
+  # print the functions names in the selected region
     for function in response['Functions']:
         print(function['FunctionName'])
 
-  # if --cleanAllLambda is specified, call the delete all tables function
+  # if --cleanAllLambda is specified, call the delete all lambda function
     if args.cleanAllLambda:
         delete_all_functions(client, response)
-    # if --functionName is specified call the delete table function
+    # if --functionName is specified call the delete lambda function
     elif args.functionName is not None:
         delete_function(client, args.functionName, response)
 
@@ -36,7 +36,7 @@ def delete_all_functions(client, response):
     #Delete all the lambda in the selected region.
     for function in response['Functions']:
         response = client.delete_function(FunctionName=function)
-        print("All the tables in the selected region have been deleted")
+        print("All the lambda resources have been deleted")
 
 # This function deletes only the lambda function that was entered  command line argument
 def delete_function(client, functionName, response):
