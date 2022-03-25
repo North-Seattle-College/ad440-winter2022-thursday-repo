0f2228a7776d9747810429b05574a8a12509e582
1jc <34551673+1jc@users.noreply.github.com>
2022-03-25 11:59:00 -0700

    Update Issue#152Sprint5DeleteAllS3Buckets..py
    1jc deleted lines 15 & 16 for Issue#152Sprint5DeleteAllS3Buckets.py   (5th commit ?)

--- Issue#152Sprint5DeleteAllS3Buckets..py
+++ Issue#152Sprint5DeleteAllS3Buckets..py
@@ -12,8 +12,8 @@ import boto3
 
 client = boto3.client('s3')
 s3 = boto3.resource('s3')
-for bucket in s3.buckets.all():
-    print(bucket.name)
+
+    
 for bucket in response['Buckets']:
     s3 = boto3.resource('s3')
     s3_bucket = s3.Bucket(bucket['Name'])
