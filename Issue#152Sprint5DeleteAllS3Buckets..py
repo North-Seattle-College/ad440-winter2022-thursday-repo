c5afc43aaf9a2c5d3bbd8ce54ba9ca4746a42cc7
1jc <34551673+1jc@users.noreply.github.com>
2022-03-25 09:51:28 -0700

    Issue#152Sprint5DeleteAllS3Buckets.py
    1jc added python script for Issue#152Sprint5DeleteAllS3Buckets.py

---
+++ Issue#152Sprint5DeleteAllS3Buckets..py
@@ -0,0 +1,24 @@
+#For deleting all s3 buckets Issue#152 Pull #177
+
+#Step 1 :- export your profile using below command Or you can export access_key and secrete_access_key 
+
+#export AWS_PROFILE=<Your-Profile-Name> 
+ 
+#Step 2:- Use below python code, Run it on local and see your all s3 buckets will delete.
+
+#If you see error like boto3 not found please go to link and install it Install boto3 using pip
+
+import boto3
+
+client = boto3.client('s3', Region='us-west-2')
+response = client.list_buckets()
+for bucket in response['Buckets']:
+    s3 = boto3.resource('s3')
+    s3_bucket = s3.Bucket(bucket['Name'])
+    bucket_versioning = s3.BucketVersioning(bucket['Name'])
+    if bucket_versioning.status == 'Enabled':
+        s3_bucket.object_versions.delete()
+    else:
+        s3_bucket.objects.all().delete()
+    response = client.delete_bucket(Bucket=bucket['Name'])
+
